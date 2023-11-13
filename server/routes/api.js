2815d483e76bb10420d48715f8bb780a3b579069
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-11-13 18:48:31 +0400

    added registration

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
+const User = require("../models/user");
 
 // Connect to MongoDB database using Mongoose
-const db = "mongodb+srv://Matthewkk01:Mate1010@cluster0.3uuxnhv.mongodb.net/";
-mongoose.connect(db);
+
+const db =
+  "mongodb+srv://Matthewkk01:Mate1010@cluster0.3uuxnhv.mongodb.net/VegaTravel";
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
 
 router.get("/", (req, res) => {
   res.send("Hello API!");
 });
+
+router.post("/register", (req, res) => {
+  let userData = req.body;
+
+  // Create a new User instance using the extracted data
+  let user = new User(userData);
+
+  //save user
+  user
+    .save()
+    .then((registeredUser) => {
+      res.status(200).send(registeredUser);
+    })
+    .catch((err) => {
+      res.status(400).send("Registration failed. Error: " + err);
+    });
+});
+
+
 
 module.exports = router;
