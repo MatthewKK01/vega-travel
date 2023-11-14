d92e0af6fd41f9786b642403436b852ed362d9a6
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-11-14 20:09:08 +0400

    created login

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -6,9 +6,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
   private _registerURL = "http://localhost:8080/api/register"
+  private _loginURL = "http://localhost:8080/api/login"
   constructor(private http: HttpClient) { }
 
   registerUser(user: any) {
     return this.http.post<any>(this._registerURL, user);
+  }
+  loginUser(user: any) {
+    return this.http.post<any>(this._loginURL, user)
   }
 }
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  loginUserData: any = {}
+  constructor(private _auth: AuthService) { }
+  loginUser() {
+    this._auth.loginUser(this.loginUserData).subscribe(
+      {
+        next: (res) => console.log(res),
+        error: err => console.log(err)
+      }
+    )
+  }
 }
